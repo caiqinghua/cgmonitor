53bc124d301ffe6b07cc21bf7f8c9ab335fab740
Lin <caiqinghua@126.com>
2017-02-21 09:52:07 +0800

    add zip compress

--- build-all.sh
+++ build-all.sh
@@ -5,3 +5,12 @@ GOOS=linux GOARCH=386 go build -o bitmicro-miner-monitor-linux-i386
 GOOS=linux GOARCH=amd64 go build -o bitmicro-miner-monitor-linux-amd64
 GOOS=darwin GOARCH=386 go build -o bitmicro-miner-monitor-macos-i386
 GOOS=darwin GOARCH=amd64 go build -o bitmicro-miner-monitor-macos-amd64
+mkdir -p btmonitor
+cp -a *i386* ./btmonitor/
+cp -a *amd64* ./btmonitor/
+cp ./exampleConfig.conf ./btmonitor/
+cp ./monitor.conf ./btmonitor/
+cp -a ./templates ./btmonitor/
+cp -a ./web-root ./btmonitor/
+zip -r btmonitor.zip ./btmonitor/
+cp btmonitor.zip ~/Downloads/
